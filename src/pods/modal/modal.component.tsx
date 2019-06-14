cca7dc0efe4269428bc4aa0800053364725519f6
Oscar Garcia Masia <ogarciamasia@gmail.com>
2019-06-14 18:59:10 +0200

    some verbose

--- src/pods/modal/modal.component.tsx
+++ src/pods/modal/modal.component.tsx
@@ -17,58 +17,75 @@ export const Modal = (props: Props) => {
     >
       <div className="modal-background">
         <div className="modal">
-          <h2>{`${props.jobTitle} at ${props.companyName}`}</h2>
-          <p>
-            As a member of an Agile department working with Safe methodology, we
-            work within iterations where the following stages are repeated:
-          </p>
-          <div className="stages">
-            <section className="refinements">
-              <h4>Refinements</h4>
-              <div className="arrow-right" />
-              <div className="task-list">
-                <ul>
-                  <li>Backlog items refinement</li>
-                  <li>Analyze business impact</li>
-                  <li>Analyze technical debt</li>
-                  <li>User stories definition</li>
-                  <li>Define features to cover with Acceptance Tests</li>
-                  <li>Sprint planning</li>
-                </ul>
-              </div>
-            </section>
-            <section className="development">
-              <h4>Development</h4>
-              <div className="arrow-right" />
-              <div className="task-list">
-                <ul>
-                  <li>Daily meeting to synchroniza</li>
-                  <li>Development of new features</li>
-                  <li>Development of tests</li>
-                  <li>Refinement of following sprints</li>
-                  <li>Track SonarQube issues</li>
-                  <li>Pull requests revisions</li>
-                  <li>BugFixing</li>
-                </ul>
-              </div>
-            </section>
-            <section className="regression">
-              <h4>Regression</h4>
-              <div className="task-list">
-                <ul>
-                  <li>Stakeholders Demo</li>
-                  <li>Team retrospective</li>
-                </ul>
-              </div>
-            </section>
-          </div>
-          <div className="action-buttons">
-            <button
-              onClick={() => props.setOpenModal(false)}
-              className="closeButton"
-            >
-              Close
-            </button>
+          <div className="modal-content">
+            <h2>{`${props.jobTitle} at ${props.companyName}`}</h2>
+            <div className="modal-intro">
+              <p>
+                As a member of an Agile department trying to adopt Safe
+                methodology, we work within iterations and sprints where the
+                following stages are repeated:
+              </p>
+            </div>
+            <div className="stages">
+              <section className="refinements">
+                <h4>Refinements</h4>
+                <div className="arrow-right" />
+                <div className="task-list">
+                  <ul>
+                    <li>Backlog items refinement</li>
+                    <li>Analyze business impact</li>
+                    <li>Analyze technical debt</li>
+                    <li>User stories definition</li>
+                    <li>Acceptance Tests scope definition</li>
+                    <li>Sprint planning</li>
+                  </ul>
+                </div>
+              </section>
+              <section className="development">
+                <h4>Development</h4>
+                <div className="arrow-right" />
+                <div className="task-list">
+                  <ul>
+                    <li>Development of new features</li>
+                    <li>Development of tests</li>
+                    <li>Daily meeting to synchroniza</li>
+                    <li>Track SonarQube issues</li>
+                    <li>Pull requests revisions</li>
+                    <li>BugFixing</li>
+                  </ul>
+                </div>
+              </section>
+              <section className="regression">
+                <h4>Regression</h4>
+                <div className="task-list">
+                  <ul>
+                    <li>Stakeholders Demo</li>
+                    <li>Team retrospective</li>
+                  </ul>
+                </div>
+              </section>
+            </div>
+            <div className="modal-final">
+              <p>
+                Methodologies and processes can be better or worse implemented,
+                but it's important to emphasize that these tasks are done each
+                sprint, and they bring quality to the product.
+              </p>
+              <p>
+                The main department goal is to dismantle an old and big monolith
+                to different microservices, maintaining functionality of the
+                application. Each new feature has to be thought in that
+                direction.
+              </p>
+            </div>
+            <div className="action-buttons">
+              <button
+                onClick={() => props.setOpenModal(false)}
+                className="closeButton"
+              >
+                Close
+              </button>
+            </div>
           </div>
         </div>
       </div>
